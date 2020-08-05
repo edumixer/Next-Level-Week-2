10fb0e446f99b112f0e0edc5585efa4477fa0bed
edumixer <edu.cena.wf@gmail.com>
2020-08-05 18:17:03 -0300

    Conectando ao Banco/Estilizando

--- aulas/web/src/components/TeacherItem/index.tsx
+++ aulas/web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,56 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css'
 
-function TeacherItem() {
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/51841874?s=460&u=c08f17e0413d6953ca7cd2d081b34453440cec9d&v=4" alt="Eduardo Franco"/>
+                <img src={ teacher.avatar } alt={ teacher.name }/>
                 <div>
-                    <strong>Eduardo Franco</strong>
-                    <span>Química</span>
+                    <strong>{ teacher.name }</strong>
+                    <span>{ teacher.subject }</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melgores tecnologias de química avançada. 
-                <br/><br/>
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{ teacher.bio }</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ { teacher.cost }</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
--- aulas/web/src/pages/Landing/index.tsx
+++ aulas/web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg'
@@ -8,12 +8,24 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
+import api from '../../services/api';
 
 
 import './styles.css'
 
 
+
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -41,7 +53,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizades <img src={purpleHeartIcon} alt="Coração Roxo"/>
+                    Total de {totalConnections} conexões já realizades <img src={purpleHeartIcon} alt="Coração Roxo"/>
                 </span>
             </div>
         </div>
