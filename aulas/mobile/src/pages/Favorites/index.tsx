925dc8c83e416615b0ea0b75aab112895fc9507e
edumixer <edu.cena.wf@gmail.com>
2020-08-09 23:48:45 -0300

    versao final mobile

--- aulas/mobile/src/pages/Favorites/index.tsx
+++ aulas/mobile/src/pages/Favorites/index.tsx
@@ -1,14 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import { useFocusEffect } from '@react-navigation/native';
+
 
 import styles from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
 
 
 function Favorites() {
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response);
+                
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
     return (
         <View style={styles.container}>
         <PageHeader title="Meus Proffys favoritos"/>
+
+        <ScrollView
+            style={styles.teacherList}
+            contentContainerStyle={{
+                paddingHorizontal: 16,
+                paddingBottom: 16,
+            }}
+        >
+            {favorites.map((teacher: Teacher) => {
+                return (
+                    <TeacherItem 
+                        key={teacher.id}
+                        teacher={teacher}
+                        favorited   // No React, se eu nao indico nada, ele é TRUE
+                    />
+                )
+            })}
+        </ScrollView>
     </View>
     );
 }
--- aulas/mobile/src/pages/Landing/index.tsx
+++ aulas/mobile/src/pages/Landing/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -13,6 +14,16 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
     const { navigate } = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
+
 
     function handleNavigateToGiveClassesPage() {
         navigate('GiveClasses');
@@ -51,7 +62,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas! {' '}
+                Total de {totalConnections} conexões já realizadas! {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
